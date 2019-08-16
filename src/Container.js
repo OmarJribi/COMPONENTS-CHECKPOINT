406c14e2b3397e84542c26d5159aadf4cfe9dcb0
3jayjoun <pro.omarjribi@gmail.com>
2019-08-16 15:00:22 +0100

    done

--- src/Container.js
+++ src/Container.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import List from './ListComponent' 
 function App() {
-    let name="Omar"
+    let name="Manel"
     return (
         <div>
         <h1>Hello {name}</h1>
